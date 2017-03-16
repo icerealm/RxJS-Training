4b08a915b2e465eee08575f176b7615e5793b035
daechatorn <daechatorn.man@gmail.com>
2017-03-16 16:06:20 +0700

    Add UI Example function 5 - 10

--- es6/js/app.js
+++ es6/js/app.js
@@ -7,6 +7,9 @@ var TOPIC_TAG_END = "</p>";
 var TOPIC_EXPLAN_CODE_MESSAGE = TOPIC_TAG_START+"Code is: "+TOPIC_TAG_END;
 var TOPIC_DISPLAY_RESULT_MESSAGE = TOPIC_TAG_START+"Result is: "+TOPIC_TAG_END;
 
+var CONTENT_EXPLAN_CODE_START = "<code><pre>";
+var CONTENT_EXPLAN_CODE_END = "</code></pre>"
+
 window.onload = function() {
 
     $('#clrBtn1').click(function(){
@@ -21,7 +24,24 @@ window.onload = function() {
     $('#clrBtn4').click(function(){
         $('#viewEx4').empty();
     });
-
+    $('#clrBtn5').click(function(){
+        $('#viewEx5').empty();
+    });
+    $('#clrBtn6').click(function(){
+        $('#viewEx6').empty();
+    });
+    $('#clrBtn7').click(function(){
+        $('#viewEx7').empty();
+    });
+    $('#clrBtn8').click(function(){
+        $('#viewEx8').empty();
+    });
+    $('#clrBtn9').click(function(){
+        $('#viewEx9').empty();
+    });
+    $('#clrBtn10').click(function(){
+        $('#viewEx10').empty();
+    });
 
 
     //----------------------- OnClick execute Code--------------------------
@@ -51,32 +71,135 @@ window.onload = function() {
     });
     
 
+    //Ex5.
+    $('#exeBtn5').click(function(){
+        $('#viewEx5').append(TOPIC_DISPLAY_RESULT_MESSAGE);
+        example5();
+    });
+    
+
+    //Ex6.
+    $('#exeBtn6').click(function(){
+        $('#viewEx6').append(TOPIC_DISPLAY_RESULT_MESSAGE);
+        example6();
+    });
+    
+
+    //Ex7.
+    $('#exeBtn7').click(function(){
+        $('#viewEx7').append(TOPIC_DISPLAY_RESULT_MESSAGE);
+        example7();
+    });
+
+    //Ex8.
+    $('#exeBtn8').click(function(){
+        $('#viewEx8').append(TOPIC_DISPLAY_RESULT_MESSAGE);
+        example8();
+    });
+
+    //Ex9.
+    $('#exeBtn9').click(function(){
+        $('#viewEx9').append(TOPIC_DISPLAY_RESULT_MESSAGE);
+        example9();
+    });
+
+    //Ex10.
+    $('#exeBtn10').click(function(){
+        $('#viewEx10').append(TOPIC_DISPLAY_RESULT_MESSAGE);
+        example10();
+    });
+
 
     //------------------------- OnClick explan code --------------------------
 
     //Ex1. 
     $('#expBtn1').click(function(){
-        $('#viewEx1').append(TOPIC_EXPLAN_CODE_MESSAGE+eval("example1")); 
+        $('#viewEx1').append(TOPIC_EXPLAN_CODE_MESSAGE+
+            CONTENT_EXPLAN_CODE_START+
+                eval("example1")+
+            CONTENT_EXPLAN_CODE_END); 
     });
 
 
     //Ex2. 
     $('#expBtn2').click(function(){
-        $('#viewEx2').append(TOPIC_EXPLAN_CODE_MESSAGE+eval("example2")); 
+        $('#viewEx2').append(TOPIC_EXPLAN_CODE_MESSAGE+
+            CONTENT_EXPLAN_CODE_START+
+                eval("example2")+
+            CONTENT_EXPLAN_CODE_END); 
     });
 
 
     //Ex3. 
     $('#expBtn3').click(function(){
-        $('#viewEx3').append(TOPIC_EXPLAN_CODE_MESSAGE+eval("example3")); 
+        $('#viewEx3').append(TOPIC_EXPLAN_CODE_MESSAGE+
+            CONTENT_EXPLAN_CODE_START+
+                eval("example3")+
+            CONTENT_EXPLAN_CODE_END); 
     });
 
 
     //Ex4. 
     $('#expBtn4').click(function(){
-        $('#viewEx4').append(TOPIC_EXPLAN_CODE_MESSAGE+eval("example4")); 
+        $('#viewEx4').append(TOPIC_EXPLAN_CODE_MESSAGE+
+            CONTENT_EXPLAN_CODE_START+
+                eval("example4")+
+            CONTENT_EXPLAN_CODE_END); 
     });
 
+
+    //Ex5. 
+    $('#expBtn5').click(function(){
+        $('#viewEx5').append(TOPIC_EXPLAN_CODE_MESSAGE+
+            CONTENT_EXPLAN_CODE_START+
+                eval("example5")+
+            CONTENT_EXPLAN_CODE_END); 
+    });
+    
+
+    //Ex6. 
+    $('#expBtn6').click(function(){
+        $('#viewEx6').append(TOPIC_EXPLAN_CODE_MESSAGE+
+            CONTENT_EXPLAN_CODE_START+
+                eval("example6")+
+            CONTENT_EXPLAN_CODE_END); 
+    });
+    
+
+    //Ex7. 
+    $('#expBtn7').click(function(){
+        $('#viewEx7').append(TOPIC_EXPLAN_CODE_MESSAGE+
+            CONTENT_EXPLAN_CODE_START+
+                eval("example7")+
+            CONTENT_EXPLAN_CODE_END); 
+    });
+    
+
+    //Ex8. 
+    $('#expBtn8').click(function(){
+        $('#viewEx8').append(TOPIC_EXPLAN_CODE_MESSAGE+
+            CONTENT_EXPLAN_CODE_START+
+                eval("example8")+
+            CONTENT_EXPLAN_CODE_END); 
+    });
+    
+
+    //Ex9. 
+    $('#expBtn9').click(function(){
+        $('#viewEx9').append(TOPIC_EXPLAN_CODE_MESSAGE+
+            CONTENT_EXPLAN_CODE_START+
+                eval("example9")+
+            CONTENT_EXPLAN_CODE_END); 
+    });
+    
+
+    //Ex10. 
+    $('#expBtn10').click(function(){
+        $('#viewEx10').append(TOPIC_EXPLAN_CODE_MESSAGE+
+            CONTENT_EXPLAN_CODE_START+
+                eval("example10")+
+            CONTENT_EXPLAN_CODE_END); 
+    });
 
 
     //----------------------- Function Code ----------------------------
@@ -128,7 +251,7 @@ window.onload = function() {
     function example5(){
         Rx.Observable.range(1,10)
                  .filter(x => x%2 === 0)
-                 .subscribe( val => {console.log(val)});
+                 .subscribe( val => {$('#viewEx5').append( "<p>"+val+"</p>");});
     }
 
 
@@ -139,16 +262,16 @@ window.onload = function() {
                 let str = x.constructor.name + "[start="+ x.start + ",count=" + x._count +"]"
                 console.log(str);
                 return x;
-            }).subscribe(d => console.log(d));
+            }).subscribe(d => $('#viewEx6').append( "<p>"+d+"</p>"));
     }
 
     // Ex7. cancel sequence
     function example7(){
-        let counter$ = Rx.Observable.interval(1000);
-        var subscription1 = counter$.take(10).subscribe(x => {console.log("subscription1:" + x)});
-        var subscription2 = counter$.take(10).subscribe(x => {console.log("subscription2:" + x)});
+        let counter$ = Rx.Observable.interval(1000); 
+        var subscription1 = counter$.take(10).subscribe(x => {$('#viewEx7').append( "<p>subscription1"+x+"</p>")});
+        var subscription2 = counter$.take(10).subscribe(x => {$('#viewEx7').append( "<p>subscription2"+x+"</p>")});
         setTimeout(() => {
-            console.log("stop subscription2!!!");
+            $('#viewEx7').append( "stop subscription2!!!")
             subscription2.unsubscribe();
         }, 3000);
     }
