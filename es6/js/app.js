f97ec9b569e659d8e13fa86fba41b73130490c5c
Sittiwerapong <JSittiwerapong@dstworldwideservices.com>
2017-03-16 17:54:43 +0700

    update examples and method

--- es6/js/app.js
+++ es6/js/app.js
@@ -2,15 +2,18 @@ import Rx from 'rxjs/Rx';
 import $ from 'jquery';
 import rx from 'rx-dom';
 
-var TOPIC_TAG_START = "<br><br><p style='color:blue;font-weight:bold;'>";
-var TOPIC_TAG_END = "</p>";
-var TOPIC_EXPLAN_CODE_MESSAGE = TOPIC_TAG_START+"Code is: "+TOPIC_TAG_END;
-var TOPIC_DISPLAY_RESULT_MESSAGE = TOPIC_TAG_START+"Result is: "+TOPIC_TAG_END;
+const TOPIC_TAG_START = "<br><br><p style='color:blue;font-weight:bold;'>";
+const TOPIC_TAG_END = "</p>";
+const TOPIC_EXPLAN_CODE_MESSAGE = TOPIC_TAG_START+"Code is: "+TOPIC_TAG_END;
+const TOPIC_DISPLAY_RESULT_MESSAGE = TOPIC_TAG_START+"Result is: "+TOPIC_TAG_END;
 
-var CONTENT_EXPLAN_CODE_START = "<code><pre>";
-var CONTENT_EXPLAN_CODE_END = "</code></pre>"
+const CONTENT_EXPLAIN_CODE_START = "<code><pre>";
+const CONTENT_EXPLAIN_CODE_END = "</code></pre>"
 
 window.onload = function() {
+    var htmlPrint = function(id, msg){
+        $(id).append("<p>" + msg + "</p>");
+    }
 
     $('#clrBtn1').click(function(){
         $('#viewEx1').empty();
@@ -115,90 +118,90 @@ window.onload = function() {
     //Ex1. 
     $('#expBtn1').click(function(){
         $('#viewEx1').append(TOPIC_EXPLAN_CODE_MESSAGE+
-            CONTENT_EXPLAN_CODE_START+
+            CONTENT_EXPLAIN_CODE_START+
                 eval("example1")+
-            CONTENT_EXPLAN_CODE_END); 
+            CONTENT_EXPLAIN_CODE_END); 
     });
 
 
     //Ex2. 
     $('#expBtn2').click(function(){
         $('#viewEx2').append(TOPIC_EXPLAN_CODE_MESSAGE+
-            CONTENT_EXPLAN_CODE_START+
+            CONTENT_EXPLAIN_CODE_START+
                 eval("example2")+
-            CONTENT_EXPLAN_CODE_END); 
+            CONTENT_EXPLAIN_CODE_END); 
     });
 
 
     //Ex3. 
     $('#expBtn3').click(function(){
         $('#viewEx3').append(TOPIC_EXPLAN_CODE_MESSAGE+
-            CONTENT_EXPLAN_CODE_START+
+            CONTENT_EXPLAIN_CODE_START+
                 eval("example3")+
-            CONTENT_EXPLAN_CODE_END); 
+            CONTENT_EXPLAIN_CODE_END); 
     });
 
 
     //Ex4. 
     $('#expBtn4').click(function(){
         $('#viewEx4').append(TOPIC_EXPLAN_CODE_MESSAGE+
-            CONTENT_EXPLAN_CODE_START+
+            CONTENT_EXPLAIN_CODE_START+
                 eval("example4")+
-            CONTENT_EXPLAN_CODE_END); 
+            CONTENT_EXPLAIN_CODE_END); 
     });
 
 
     //Ex5. 
     $('#expBtn5').click(function(){
         $('#viewEx5').append(TOPIC_EXPLAN_CODE_MESSAGE+
-            CONTENT_EXPLAN_CODE_START+
+            CONTENT_EXPLAIN_CODE_START+
                 eval("example5")+
-            CONTENT_EXPLAN_CODE_END); 
+            CONTENT_EXPLAIN_CODE_END); 
     });
     
 
     //Ex6. 
     $('#expBtn6').click(function(){
         $('#viewEx6').append(TOPIC_EXPLAN_CODE_MESSAGE+
-            CONTENT_EXPLAN_CODE_START+
+            CONTENT_EXPLAIN_CODE_START+
                 eval("example6")+
-            CONTENT_EXPLAN_CODE_END); 
+            CONTENT_EXPLAIN_CODE_END); 
     });
     
 
     //Ex7. 
     $('#expBtn7').click(function(){
         $('#viewEx7').append(TOPIC_EXPLAN_CODE_MESSAGE+
-            CONTENT_EXPLAN_CODE_START+
+            CONTENT_EXPLAIN_CODE_START+
                 eval("example7")+
-            CONTENT_EXPLAN_CODE_END); 
+            CONTENT_EXPLAIN_CODE_END); 
     });
     
 
     //Ex8. 
     $('#expBtn8').click(function(){
         $('#viewEx8').append(TOPIC_EXPLAN_CODE_MESSAGE+
-            CONTENT_EXPLAN_CODE_START+
+            CONTENT_EXPLAIN_CODE_START+
                 eval("example8")+
-            CONTENT_EXPLAN_CODE_END); 
+            CONTENT_EXPLAIN_CODE_END); 
     });
     
 
     //Ex9. 
     $('#expBtn9').click(function(){
         $('#viewEx9').append(TOPIC_EXPLAN_CODE_MESSAGE+
-            CONTENT_EXPLAN_CODE_START+
+            CONTENT_EXPLAIN_CODE_START+
                 eval("example9")+
-            CONTENT_EXPLAN_CODE_END); 
+            CONTENT_EXPLAIN_CODE_END); 
     });
     
 
     //Ex10. 
     $('#expBtn10').click(function(){
         $('#viewEx10').append(TOPIC_EXPLAN_CODE_MESSAGE+
-            CONTENT_EXPLAN_CODE_START+
+            CONTENT_EXPLAIN_CODE_START+
                 eval("example10")+
-            CONTENT_EXPLAN_CODE_END); 
+            CONTENT_EXPLAIN_CODE_END); 
     });
 
 
@@ -209,7 +212,7 @@ window.onload = function() {
         let stream$ = Rx.Observable.of(1,2,3,5,6);
         console.log(stream$);
         stream$.subscribe(d => {
-            $('#viewEx1').append(d + "&nbsp;")
+            htmlPrint('#viewEx1', d +"&nbsp;");
         });
     }
 
@@ -220,19 +223,19 @@ window.onload = function() {
         let view2 = $('#viewEx2');
         let stream2$ = Rx.Observable.fromEvent(textInput, "keypress");
         stream2$.map(event => event.keyCode).subscribe( d => {
-            view2.append("<p>" + d + "</p>");
-
+            htmlPrint('#viewEx2', d);
         });
     }
 
 
     //Ex3.
     function example3(){
+        
         let stream3$ = rx.DOM.get('https://jsonplaceholder.typicode.com/posts/1');
         stream3$.map(data => JSON.parse(data.response))
-                .subscribe(model => { $('#viewEx3').append("<p>" + JSON.stringify(model) + "</p>") },
-                             err => { $('#viewEx3').append(err)},
-                             ()  => { $('#viewEx3').append("completed!!")});
+                .subscribe(model => { htmlPrint('#viewEx3', JSON.stringify(model)) },
+                             err => { htmlPrint('#viewEx3', err)},
+                             ()  => { htmlPrint('#viewEx3', "completed!!")});
     }
 
 
@@ -240,18 +243,23 @@ window.onload = function() {
     function example4(){
         Rx.Observable.range(1,5)
                     .map(x => {
-                        $('#viewEx4').append( "<p>src:" + x +"</p>");
+                        htmlPrint('#viewEx4',"src:" + x );
                         return 2*x;
                     })
-                    .subscribe(x => {$('#viewEx4').append("<p>final:" + x +"</p>")});
+                    .subscribe(x => {htmlPrint('#viewEx4', "final:" + x)});
     }
 
 
     //Ex5. Code which, use to filter
     function example5(){
         Rx.Observable.range(1,10)
-                 .filter(x => x%2 === 0)
-                 .subscribe( val => {$('#viewEx5').append( "<p>"+val+"</p>");});
+                 .filter(x => {
+                    let rs = x%2 === 0;
+                    let msg = "rs:" + rs + ",val=" + x;
+                    htmlPrint('#viewEx5', msg);
+                    return rs;
+                 })
+                 .subscribe( val => {htmlPrint('#viewEx5',"final:"+val);});
     }
 
 
@@ -260,19 +268,27 @@ window.onload = function() {
         let source$ = Rx.Observable.range(1, 3).map(x => Rx.Observable.range(x, 4));
         source$.flatMap(x => {
                 let str = x.constructor.name + "[start="+ x.start + ",count=" + x._count +"]"
-                console.log(str);
+                htmlPrint('#viewEx6', str);
                 return x;
-            }).subscribe(d => $('#viewEx6').append( "<p>"+d+"</p>"));
+            }).subscribe(d => htmlPrint('#viewEx6', d));
     }
 
     // Ex7. cancel sequence
     function example7(){
         let counter$ = Rx.Observable.interval(1000); 
-        var subscription1 = counter$.take(10).subscribe(x => {$('#viewEx7').append( "<p>subscription1"+x+"</p>")});
-        var subscription2 = counter$.take(10).subscribe(x => {$('#viewEx7').append( "<p>subscription2"+x+"</p>")});
+        var subscription1 = counter$.take(8).subscribe(x => {htmlPrint('#viewEx7', "subscription1:"+x)});
+        var subscription2 = counter$.take(8).subscribe(x => {htmlPrint('#viewEx7', "subscription2:"+x)});
         setTimeout(() => {
-            $('#viewEx7').append( "stop subscription2!!!")
+            htmlPrint('#viewEx7',"stop subscription2!!!")
             subscription2.unsubscribe();
         }, 3000);
     }
+
+    // Ex8. retry sending request
+    function example8(){
+        rx.DOM.get("https://jsonplaceholder.typicode.com/posts/2")
+                .retry(3)
+                .subscribe(x => htmlPrint('#viewEx8', JSON.stringify(x.response)));
+    }
+
 }
