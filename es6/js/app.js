d33e917adf1286bb1f03acf6681868e4b12d7bf3
Sittiwerapong <JSittiwerapong@dstworldwideservices.com>
2017-03-17 18:41:33 +0700

    add more examples

--- es6/js/app.js
+++ es6/js/app.js
@@ -45,6 +45,9 @@ window.onload = function() {
     $('#clrBtn10').click(function(){
         $('#viewEx10').empty();
     });
+    $('#clrBtn11').click(function(){
+        $('#viewEx11').empty();
+    });
 
 
     //----------------------- OnClick execute Code--------------------------
@@ -112,8 +115,14 @@ window.onload = function() {
         example10();
     });
 
+    //Ex11.
+    $('#exeBtn11').click(function(){
+        $('#viewEx11').append(TOPIC_DISPLAY_RESULT_MESSAGE);
+        example11();
+    });
 
-    //------------------------- OnClick explan code --------------------------
+
+    //------------------------- OnClick explain code --------------------------
 
     //Ex1. 
     $('#expBtn1').click(function(){
@@ -204,43 +213,50 @@ window.onload = function() {
             CONTENT_EXPLAIN_CODE_END); 
     });
 
+    //Ex11. 
+    $('#expBtn11').click(function(){
+        $('#viewEx11').append(TOPIC_EXPLAN_CODE_MESSAGE+
+            CONTENT_EXPLAIN_CODE_START+
+                eval("example11")+
+            CONTENT_EXPLAIN_CODE_END); 
+    });
+
 
     //----------------------- Function Code ----------------------------
 
-    //Ex1. Code which, use to create Observable items
     function example1(){
-        let stream$ = Rx.Observable.of(1,2,3,5,6);
-        console.log(stream$);
-        stream$.subscribe(d => {
+        //Ex1. Use of to create Observable items
+        let source$ = Rx.Observable.of(1,2,3,5,6);
+        console.log(source$);
+        source$.subscribe(d => {
             htmlPrint('#viewEx1', d +"&nbsp;");
         });
     }
 
 
-    //Ex2. Code which, observe the input box when user fill in data.
     function example2(){
+            //Ex2. Observe the input box when user fill in data.
         let textInput = $('#dataIn');
         let view2 = $('#viewEx2');
-        let stream2$ = Rx.Observable.fromEvent(textInput, "keypress");
-        stream2$.map(event => event.keyCode).subscribe( d => {
+        let source$ = Rx.Observable.fromEvent(textInput, "keypress");
+        source$.map(event => event.keyCode).subscribe( d => {
             htmlPrint('#viewEx2', d);
         });
     }
 
 
-    //Ex3.
     function example3(){
-        
-        let stream3$ = rx.DOM.get('https://jsonplaceholder.typicode.com/posts/1');
-        stream3$.map(data => JSON.parse(data.response))
+        //Ex3. Use rx.DOM to create http.get as source  
+        let source$ = rx.DOM.get('https://jsonplaceholder.typicode.com/posts/1');
+        source$.map(data => JSON.parse(data.response))
                 .subscribe(model => { htmlPrint('#viewEx3', JSON.stringify(model)) },
                              err => { htmlPrint('#viewEx3', err)},
                              ()  => { htmlPrint('#viewEx3', "completed!!")});
     }
 
-
-    //Ex4. Code which, use to map
+    
     function example4(){
+        //Ex4. Use map to transform source to desired data
         Rx.Observable.range(1,5)
                     .map(x => {
                         htmlPrint('#viewEx4',"src:" + x );
@@ -250,8 +266,8 @@ window.onload = function() {
     }
 
 
-    //Ex5. Code which, use to filter
     function example5(){
+        //Ex5. use filter to filter out source. 
         Rx.Observable.range(1,10)
                  .filter(x => {
                     let rs = x%2 === 0;
@@ -263,8 +279,9 @@ window.onload = function() {
     }
 
 
-    // Ex6. flatMap
+    
     function example6(){
+        // Ex6. flatMap
         let source$ = Rx.Observable.range(1, 3).map(x => Rx.Observable.range(x, 4));
         source$.flatMap(x => {
                 let str = x.constructor.name + "[start="+ x.start + ",count=" + x._count +"]"
@@ -273,8 +290,9 @@ window.onload = function() {
             }).subscribe(d => htmlPrint('#viewEx6', d));
     }
 
-    // Ex7. cancel sequence
+    
     function example7(){
+        // Ex7. cancel sequence
         let counter$ = Rx.Observable.interval(1000); 
         var subscription1 = counter$.take(8).subscribe(x => {htmlPrint('#viewEx7', "subscription1:"+x)});
         var subscription2 = counter$.take(8).subscribe(x => {htmlPrint('#viewEx7', "subscription2:"+x)});
@@ -283,12 +301,82 @@ window.onload = function() {
             subscription2.unsubscribe();
         }, 3000);
     }
-
-    // Ex8. retry sending request
+    
     function example8(){
+        // Ex8. retry sending request
         rx.DOM.get("https://jsonplaceholder.typicode.com/posts/2")
                 .retry(3)
                 .subscribe(x => htmlPrint('#viewEx8', JSON.stringify(x.response)));
     }
+
+
+    function example9(){
+        //Ex9. merge
+        let source1$ = Rx.Observable.of("src1: " + 1, "src1: " + 2, "src1: " + 3);
+        let source2$ = Rx.Observable.of("src2: " + 4, "src2: " + 5, "src2: " + 6);
+
+        let source$ = Rx.Observable.merge(source1$, source2$);
+        source$.subscribe(x => htmlPrint("#viewEx9", x));
+    }
+
+    function example10(){
+        //Ex10. subject is both Observable and Observer.
+        let datasource$ = Rx.Observable.interval(200)
+                                    .map(x => "datasource:" + x)
+                                    .take(10);
+        let subject = new Rx.Subject();
+        datasource$.subscribe(subject);
+
+        subject.subscribe(
+            data  => {htmlPrint("#viewEx10", "observer:" + data)},
+            error => {htmlPrint("#viewEx10", error)},
+            ()    => {htmlPrint("#viewEx10", "observer completed")}
+        );
+
+        subject.next("subject msg#1");
+        setTimeout(() => {
+            subject.next("subject msg#2");
+        }, 800)
+        
+
+        setTimeout(() => {
+            subject.complete();
+        }, 1200);
+    }
+
+    function example11(){
+        //Ex11. AsyncSubject emits the last value if there is complete notification.
+        let data = 0;
+        htmlPrint("#viewEx11", "initial data:" + data);
+        let datasource$ = Rx.Observable.create( obs =>{
+            obs.next(++data);
+            setTimeout(() => obs.next(++data), 1000);
+            setTimeout(() => obs.next(++data), 2000);
+            setTimeout(() => obs.complete(), 2500);
+        })
+        let subject = new Rx.AsyncSubject();
+
+        datasource$.subscribe(subject);
+
+        subject.subscribe(x => htmlPrint("#viewEx11", "onNext, data:" + x),
+                        err => htmlPrint("#viewEx11", err),
+                        ()  => {
+                                htmlPrint("#viewEx11", 'completed, data:'+ data);
+                               });
+        htmlPrint("#viewEx11", "before code ending, data:" + data);
+    }
+
+    function example12(){
+        let subject = new Rx.BehaviorSubject("Loading page...");
+
+        subject.subscribe( x => console.log(x || "empty"),
+                        err => console.log(err));
+        Rx.Observable.create(obs => {
+            setTimeout(() => obs.next("{data:'myscript'}"), 2000);
+            setTimeout(() => obs.next("{data:'myscript2'}"), 3000);
+            setTimeout(() => obs.next(null), 3000);
+        }).subscribe(subject);
+    }
+
 
 }
